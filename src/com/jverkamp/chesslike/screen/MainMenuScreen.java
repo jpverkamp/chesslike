b50e51621f4b0d935ce13f07272aabea18125a22
JP Verkamp <verkampj@indiana.edu>
2013-02-01 01:52:36 -0500

    Added levels defined by a text file, base text files, and an indicator of where enemies can move.

--- src/com/jverkamp/chesslike/screen/MainMenuScreen.java
+++ src/com/jverkamp/chesslike/screen/MainMenuScreen.java
@@ -24,12 +24,22 @@ public class MainMenuScreen extends Screen {
 		if (code == KeyEvent.VK_ENTER) {
 			List<Actor> yourHumbleArmy = new ArrayList<Actor>();
 			yourHumbleArmy.add(new King(null, 0));
-			yourHumbleArmy.add(new Archbishop(null, 0));
-			yourHumbleArmy.add(new Marshall(null, 0));
-			yourHumbleArmy.add(new Queen(null, 0));
-			yourHumbleArmy.add(new Unicorn(null, 0));
 			
-			return new WorldIntroScreen(new WorldScreen(yourHumbleArmy, Color.WHITE, 7));
+			// Start level
+			return new WorldIntroScreen(new WorldScreen(yourHumbleArmy, Color.WHITE, 1)); // Forest
+//			
+//			return new WorldIntroScreen(new WorldScreen(yourHumbleArmy, Color.WHITE, 2)); // Caves
+//			return new WorldIntroScreen(new WorldScreen(yourHumbleArmy, Color.GREEN, 2)); // Underground Lake
+//			return new WorldIntroScreen(new WorldScreen(yourHumbleArmy, Color.BLUE, 2)); // Underground Forest
+//			
+//			return new WorldIntroScreen(new WorldScreen(yourHumbleArmy, Color.WHITE, 6)); // Dungeon
+//			return new WorldIntroScreen(new WorldScreen(yourHumbleArmy, new Color(255, 215, 0) /* GOLD */, 7)); // Cathedral
+//			return new WorldIntroScreen(new WorldScreen(yourHumbleArmy, new Color(183, 65, 14) /* RUST */, 7)); // Foundry
+//			
+//			return new WorldIntroScreen(new WorldScreen(yourHumbleArmy, Color.WHITE, 10)); // Sunken City
+//			
+//			return new WorldIntroScreen(new WorldScreen(yourHumbleArmy, Color.WHITE, 13)); // Throne Room
+
 		} else if (event.getKeyCode() == KeyEvent.VK_ESCAPE) {
 			return null;
 		} else if (event.getKeyCode() == KeyEvent.VK_F1){
